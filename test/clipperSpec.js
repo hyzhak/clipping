af53b934f2c937e8afca20f8d960c01b663e8a7a
Eugene Krevenets <hyzhak@ya.ru>
2015-02-20 02:48:44 +0100

    install mocha and chai for testing

---
+++ test/clipperSpec.js
@@ -0,0 +1,8 @@
+var clipper = require('../index'),
+    expect = require('chai').expect;
+
+describe('Clipper', function() {
+    it('should be ....', function() {
+        expect(clipper).to.not.be.a('string');
+    });
+});
