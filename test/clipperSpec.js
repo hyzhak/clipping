ac96288054047784ec617d7cef9b3b8d5346c2bb
Eugene Krevenets <hyzhak@ya.ru>
2015-02-21 01:40:21 +0100

    should work fine with undefined argument

--- test/clipperSpec.js
+++ test/clipperSpec.js
@@ -2,7 +2,11 @@ var clipper = require('../index'),
     expect = require('chai').expect;
 
 describe('Clipper', function() {
-    it('should be "you" convert to "U"', function() {
+    it('should return null on undefined', function() {
+        expect(clipper()).to.equal('');
+    });
+
+    it('should "you" convert to "U"', function() {
         expect(clipper('you')).to.equal('U');
     });
 });
