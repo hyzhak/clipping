0a893243a32b2159c41fbfe0314b7bcd580e849d
Eugene Krevenets <hyzhak@ya.ru>
2015-03-28 00:51:47 +0100

    add google analytics

--- example/src/js/outputMessage.jsx
+++ example/src/js/outputMessage.jsx
@@ -2,6 +2,9 @@ var React = require('react'),
     ReactZeroClipboard = require('react-zeroclipboard');
 
 module.exports = React.createClass({
+    sendStatistics() {
+          ga('send', 'event', 'copy', this.props.value);
+    },
     render(): any {
         return <div className="input-group">
             <input type="text"
@@ -10,7 +13,8 @@ module.exports = React.createClass({
                 placeholder="Type your message"
                 readOnly="true"
                 value={this.props.value} />
-            <ReactZeroClipboard text={this.props.value} className="input-group-addon">
+            <ReactZeroClipboard text={this.props.value} className="input-group-addon"
+                onCopy={this.sendStatistics}>
                 Copy
             </ReactZeroClipboard>
         </div>;
